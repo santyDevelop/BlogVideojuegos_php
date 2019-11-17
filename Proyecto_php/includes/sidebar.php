e207ce079e56cd8a7e6bb51e85fd510bdb8f52dc
Santy <Santy@DESKTOP-A228AC9>
2019-11-17 18:27:27 +0100

    Programacion del registro de usuarios: - Obtencion de campos del formulario - Validacion de campos del formulario - Insercion en la base de datos en tabla usuarios - Mostrar alertas en la vista

--- Proyecto_php/includes/sidebar.php
+++ Proyecto_php/includes/sidebar.php
@@ -1,3 +1,5 @@
+<?php require_once 'alertLib.php';?>
+
 <!--BARRA LATERAL-->
 <aside id="sidebar">
 
@@ -16,22 +18,39 @@
     </div>
 
     <!--REGISTRO-->
-    <div id="registrar" class="bloque">
+    <div id="registrar" class="bloque">        
         <h3>Registrate</h3>
-        <form action="registro.php" method="POST">
+        
+        <?php if(isset($_SESSION['registroExito'])): ?>  
+            <div class="alerta alerta-exito">
+                <?= $_SESSION['registroExito'] ?>
+            </div>
+        <?php elseif(isset($_SESSION['errores']['registroFallo'])): ?> 
+            <div class="alerta alerta-error">
+                <?= $_SESSION['errores']['registroFallo'] ?>
+            </div>
+        <?php endif; ?> 
+        
+        
+        <form action="Proyecto_php/funcionalidad/registro.php" method="POST">
             <label for="name">Nombre</label>
             <input type="text" name="name"/>
+            <?php echo isset($_SESSION['errores']) ? mostrarError($_SESSION['errores'], 'nombre') : ''; ?>            
 
             <label for="apellidos">Apellidos</label>
             <input type="text" name="apellidos"/>
-
+            <?php echo isset($_SESSION['errores']) ? mostrarError($_SESSION['errores'], 'apellidos') : ''; ?> 
+            
             <label for="email">Email</label>
             <input type="email" name="email"/>
-
+            <?php echo isset($_SESSION['errores']) ? mostrarError($_SESSION['errores'], 'email') : ''; ?> 
+            
             <label for="password">Contraseña</label>
             <input type="password" name="password"/>
-
-            <input type="submit" value="Registrar"/>
+            <?php echo isset($_SESSION['errores']) ? mostrarError($_SESSION['errores'], 'password') : ''; ?> 
+            
+            <input type="submit" name="submit" value="Registrar"/>
         </form>
+        <?php borrarErrores(); ?>
     </div>
 </aside>
